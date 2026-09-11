77b4151f6ace24a80648c8e18d296a638161aa96
Adam12389901 <128Akonikov@frhsd.com>
2026-09-11 09:37:48 -0400

    Reply two

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("lz");
         System.out.println("Hi lz, I am MML");
+        System.out.println("Hi lz MML, I am AK");
     }
 }
